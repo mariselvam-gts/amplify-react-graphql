f9d9f807aab1e86e5357fbb3f4e951224d52c311
Mariselvam Lakshmanasamy <mariselvam@gabriels.net>
2024-04-13 22:54:41 +0530

    Test update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Mariselvam!
+          Hello Mariselvam! Welcome to Amplify!
         </p>
         <a
           className="App-link"
